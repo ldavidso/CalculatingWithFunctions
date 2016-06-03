0e635959e624f516970657ad4f5a747c03eda631
Lawrence Davidson <lawrencex2@gmail.com>
2016-06-03 19:19:29 -0400

    Refactor number functions.

--- scripts/calculate.js
+++ scripts/calculate.js
@@ -7,36 +7,16 @@ function proc(n, fn) {
     }
 }
 
-function zero(fn) {
-    return proc(0, fn);
-}
-function one(fn) {
-    return proc(1, fn);
-}
-function two(fn) {
-    return proc(2, fn);
-}
-function three(fn) {
-    return proc(3, fn);
-}
-function four(fn) {
-    return proc(4, fn);
-}
-function five(fn) {
-    return proc(5, fn);
-}
-function six(fn) {
-    return proc(6, fn);
-}
-function seven(fn) {
-    return proc(7, fn);
-}
-function eight(fn) {
-    return proc(8, fn);
-}
-function nine(fn) {
-    return proc(9, fn);
-}
+function zero(fn)  { return proc(0, fn); }
+function one(fn)   { return proc(1, fn); }
+function two(fn)   { return proc(2, fn); }
+function three(fn) { return proc(3, fn); }
+function four(fn)  { return proc(4, fn); }
+function five(fn)  { return proc(5, fn); }
+function six(fn)   { return proc(6, fn); }
+function seven(fn) { return proc(7, fn); }
+function eight(fn) { return proc(8, fn); }
+function nine(fn)  { return proc(9, fn); }
 
 function plus(num) {
     return function(res) { return res + num; }
